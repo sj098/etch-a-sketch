a95caccdf56249a51c842b97c6bf1bb7faa8129b
Suvin Jeon <suvinjeon@gmail.com>
2022-09-07 19:03:46 -0400

    Code for change in background color at mouseover

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const container = document.querySelector('.container');
 
+
 for (let i = 0; i < 16; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+    const squares = document.createElement('div');
+    squares.classList.add('squares');
+    container.appendChild(squares);
 }
+
+let squares = container.childNodes;
+
+function change(a) {
+    a.style.backgroundColor = 'lightgrey';
+}
+
+squares.forEach((square) => square.addEventListener("mouseenter", function() {
+    change(square);
+}));

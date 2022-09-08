dba8b67e66b134106f184ee74e33c52e5107907d
Suvin Jeon <suvinjeon@gmail.com>
2022-09-08 19:35:11 -0400

    Add function to change grid size

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const body = document.querySelector('.body');
 const container = document.querySelector('.container');
-let gridsize = 4;
+let gridsize = 16;
+let squares = container.childNodes;
 
 function grid() {
     for (let i = 0; i < gridsize * gridsize; i++) {
@@ -8,24 +9,40 @@ function grid() {
         squares.classList.add('squares');
         squares.style.width = '' + (400 / gridsize - 2) + 'px';
         squares.style.height = '' + (400 / gridsize - 2) + 'px';
-        container.append(squares);
+        container.appendChild(squares);
     }
- }    
-
-grid();
-
-let squares = container.childNodes;
+}    
 
 function change(a) {
     a.style.backgroundColor = 'black';
 }
 
-squares.forEach((square) => square.addEventListener("mouseenter", function() {
-    change(square);
-}));
-
-document.getElementById('button').onclick = function() {
-    let popup = prompt("Enter grid size: ")
-    
+function sketch() {
+    squares.forEach((square) => square.addEventListener("mouseenter", function() {
+        change(square);
+    }))
 }
 
+function clear() {
+    for (let i = 0; i < gridsize * gridsize; i++) {
+        container.removeChild(container.lastElementChild);
+}}
+
+
+document.getElementById('button').onclick = function() {
+    let popup = prompt("Enter a number between 1 and 100.")
+    if (popup < 1 || popup > 100 || isNaN(popup)) (
+        prompt("Sorry, I didn't get that. Please enter a number from 1 to 100.")
+    )
+    else if (popup >= 1 && popup <= 100) {
+        clear();
+        gridsize = popup;
+        grid();
+        sketch()
+    }
+}   
+
+
+
+grid();
+sketch();

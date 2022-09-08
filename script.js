8909b8ef8fea868a8bc257843f44b014bd80921a
Suvin Jeon <suvinjeon@gmail.com>
2022-09-08 19:12:55 -0400

    Change method grid is created from CSS grid to Flexbox

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 const body = document.querySelector('.body');
 const container = document.querySelector('.container');
+let gridsize = 4;
 
-const button = document.createElement('button');
-button.classList.add('button');
-button.textContent = 'Grid Size';
-body.appendChild(button);
+function grid() {
+    for (let i = 0; i < gridsize * gridsize; i++) {
+        const squares = document.createElement('div');
+        squares.classList.add('squares');
+        squares.style.width = '' + (400 / gridsize - 2) + 'px';
+        squares.style.height = '' + (400 / gridsize - 2) + 'px';
+        container.append(squares);
+    }
+ }    
 
-for (let i = 0; i < 16; i++) {
-    const squares = document.createElement('div');
-    squares.classList.add('squares');
-    container.appendChild(squares);
-}
+grid();
 
 let squares = container.childNodes;
 
 function change(a) {
-    a.style.backgroundColor = 'lightgrey';
+    a.style.backgroundColor = 'black';
 }
 
 squares.forEach((square) => square.addEventListener("mouseenter", function() {
     change(square);
 }));
+
+document.getElementById('button').onclick = function() {
+    let popup = prompt("Enter grid size: ")
+    
+}
+
